f306105b7f9bf23900537a2cf672b4de9ac9034f
Trevor Gowing <trevor@decodedev.co.za>
2021-11-17 16:34:19 +0200

    Bind WifiP2pBroadcastReceiver to MainAcitivity

--- app/src/main/java/com/trevorgowing/android/p2p/MainActivity.kt
+++ app/src/main/java/com/trevorgowing/android/p2p/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.trevorgowing.android.p2p
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
 import android.net.NetworkInfo
 import android.net.wifi.p2p.WifiP2pDevice
 import android.net.wifi.p2p.WifiP2pDeviceList
+import android.net.wifi.p2p.WifiP2pManager
 import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -19,6 +24,12 @@ class MainActivity : AppCompatActivity() {
 
   private lateinit var appBarConfiguration: AppBarConfiguration
   private lateinit var binding: ActivityMainBinding
+
+  // Wifi P2p
+  private val wifiP2pManager: WifiP2pManager by
+    lazy(LazyThreadSafetyMode.NONE) { getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager }
+  var wifiP2pChannel: WifiP2pManager.Channel? = null
+  var wifiP2pReceiver: BroadcastReceiver? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -34,8 +45,35 @@ class MainActivity : AppCompatActivity() {
 
     binding.fab.setOnClickListener { view ->
       Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        .setAction("Action", null).show()
+        .setAction("Action", null)
+        .show()
     }
+
+    // Wifi P2p
+    wifiP2pChannel = wifiP2pManager.initialize(this, mainLooper, null)
+    wifiP2pChannel?.also { channel ->
+      wifiP2pReceiver = WifiP2pBroadcastReceiver(wifiP2pManager, channel, this)
+    }
+  }
+
+  override fun onResume() {
+    super.onResume()
+    wifiP2pReceiver?.also {
+      registerReceiver(
+        it,
+        IntentFilter().apply {
+          addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+          addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+          addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+          addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+        }
+      )
+    }
+  }
+
+  override fun onPause() {
+    super.onPause()
+    wifiP2pReceiver?.also { unregisterReceiver(it) }
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
