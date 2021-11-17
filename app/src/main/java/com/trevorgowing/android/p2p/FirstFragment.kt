4b9eafbb9a162a1200f3518c48b610eef63f60c6
Trevor Gowing <trevor@decodedev.co.za>
2021-11-17 15:34:41 +0200

    Delete SecondFragment

--- app/src/main/java/com/trevorgowing/android/p2p/FirstFragment.kt
+++ app/src/main/java/com/trevorgowing/android/p2p/FirstFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.trevorgowing.android.p2p.databinding.FragmentFirstBinding
 
 /**
@@ -22,19 +21,13 @@ class FirstFragment : Fragment() {
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View? {
-
+  ): View {
     _binding = FragmentFirstBinding.inflate(inflater, container, false)
     return binding.root
-
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-
-    binding.buttonFirst.setOnClickListener {
-      findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-    }
   }
 
   override fun onDestroyView() {
