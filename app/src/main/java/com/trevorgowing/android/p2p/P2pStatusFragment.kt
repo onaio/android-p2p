78a263522bf90aeb134b2d6f1ef762191076cac5
Trevor Gowing <trevor@decodedev.co.za>
2021-11-17 16:37:54 +0200

    Rename FirstFragment to P2pStatusFragement

--- app/src/main/java/com/trevorgowing/android/p2p/P2pStatusFragment.kt
+++ app/src/main/java/com/trevorgowing/android/p2p/P2pStatusFragment.kt
@@ -5,14 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.trevorgowing.android.p2p.databinding.FragmentFirstBinding
+import com.trevorgowing.android.p2p.databinding.FragmentP2pStatusBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class P2pStatusFragment : Fragment() {
 
-  private var _binding: FragmentFirstBinding? = null
+  private var _binding: FragmentP2pStatusBinding? = null
 
   // This property is only valid between onCreateView and
   // onDestroyView.
@@ -22,7 +22,7 @@ class FirstFragment : Fragment() {
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    _binding = FragmentFirstBinding.inflate(inflater, container, false)
+    _binding = FragmentP2pStatusBinding.inflate(inflater, container, false)
     return binding.root
   }
 
