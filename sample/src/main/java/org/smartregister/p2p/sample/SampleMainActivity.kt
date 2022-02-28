1cca2d542ca18d1da8e4a7b9cb3b7bc212c73e86
Richard Kareko <rkareko@ona.io>
2022-02-28 07:09:13 +0300

    Add P2P app as a project depency to saple app

--- sample/src/main/java/org/smartregister/p2p/sample/SampleMainActivity.kt
+++ sample/src/main/java/org/smartregister/p2p/sample/SampleMainActivity.kt
@@ -1,11 +1,15 @@
 package org.smartregister.p2p.sample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.smartregister.p2p.MainActivity
 
-class MainActivity : AppCompatActivity() {
+class SampleMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, MainActivity::class.java) )
     }
 }
