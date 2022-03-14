a13666afbbf69896a2ec90455916304858d4b9c3
Richard Kareko <rkareko@ona.io>
2022-03-14 18:29:17 +0300

    Add comment

--- p2p-lib/src/main/java/org/smartregister/p2p/search/contract/P2pModeSelectContract.kt
+++ p2p-lib/src/main/java/org/smartregister/p2p/search/contract/P2pModeSelectContract.kt
@@ -2,6 +2,9 @@ package org.smartregister.p2p.search.contract
 
 import org.smartregister.p2p.authentication.model.DeviceRole
 
+/**
+ * Interface for functions used to make changes to the data transfer page UI
+ */
 interface P2pModeSelectContract {
 
     fun showP2PSelectPage(deviceRole: DeviceRole, deviceName: String)
