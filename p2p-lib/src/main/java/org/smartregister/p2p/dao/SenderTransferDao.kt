e369c515f82ec0695379103b965af9917593d516
Richard Kareko <rkareko@ona.io>
2022-03-21 08:25:58 +0300

    Run spotless apply

--- p2p-lib/src/main/java/org/smartregister/p2p/dao/SenderTransferDao.kt
+++ p2p-lib/src/main/java/org/smartregister/p2p/dao/SenderTransferDao.kt
@@ -1,4 +1,31 @@
+/*
+ * Copyright 2022 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.smartregister.p2p.dao
 
+import androidx.annotation.NonNull
+import androidx.annotation.Nullable
+import androidx.annotation.WorkerThread
+import java.util.TreeSet
+import org.smartregister.p2p.search.data.JsonData
+import org.smartregister.p2p.sync.DataType
+
 interface SenderTransferDao {
+  @WorkerThread @Nullable fun getDataTypes(): TreeSet<DataType?>?
+
+  @WorkerThread
+  @Nullable
+  fun getJsonData(@NonNull dataType: DataType?, lastRecordId: Long, batchSize: Int): JsonData?
 }
