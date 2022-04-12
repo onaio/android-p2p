d989f62bbe5c4809c5b64176eaa6dfdac6ac862c
Richard Kareko <rkareko@ona.io>
2022-04-12 16:18:00 +0300

    Intantiate P2PLibrary in sample activity

--- sample/src/main/java/org/smartregister/p2p/sample/SampleMainActivity.kt
+++ sample/src/main/java/org/smartregister/p2p/sample/SampleMainActivity.kt
@@ -3,12 +3,23 @@ package org.smartregister.p2p.sample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import org.smartregister.p2p.P2PLibrary
+import org.smartregister.p2p.dao.SenderTransferDao
+import org.smartregister.p2p.sample.dao.P2PSenderTransferDao
 import org.smartregister.p2p.utils.startP2PScreen
 
 class SampleMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample_main)
+
+        // Init P2PLibrary
+        val p2POptions = P2PLibrary.Options(context = this,
+            dbPassphrase = "demo",
+            username = "demo",
+        senderTransferDao = P2PSenderTransferDao()
+        )
+        P2PLibrary().init(p2POptions)
 
         findViewById<Button>(R.id.device_sync).setOnClickListener {
             startP2PScreen(this)
--- sample/src/main/java/org/smartregister/p2p/sample/dao/P2PSenderTransferDao.kt
+++ sample/src/main/java/org/smartregister/p2p/sample/dao/P2PSenderTransferDao.kt
@@ -1,4 +1,16 @@
 package org.smartregister.p2p.sample.dao
 
-class P2PSenderTransferDao {
+import org.smartregister.p2p.dao.SenderTransferDao
+import org.smartregister.p2p.search.data.JsonData
+import org.smartregister.p2p.sync.DataType
+import java.util.*
+
+class P2PSenderTransferDao() :SenderTransferDao {
+    override fun getP2PDataTypes(): TreeSet<DataType> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getJsonData(dataType: DataType, lastRecordId: Long, batchSize: Int): JsonData? {
+        TODO("Not yet implemented")
+    }
 }
