07aee864913d4350ae4f20f142be1ca583ca7f9c
Richard Kareko <rkareko@ona.io>
2022-09-20 15:45:17 +0300

    Initialize wifiP2pChannel in P2PDeviceSearchActivity onResume

--- p2p-lib/src/main/java/org/smartregister/p2p/data_sharing/DataSharingStrategy.kt
+++ p2p-lib/src/main/java/org/smartregister/p2p/data_sharing/DataSharingStrategy.kt
@@ -95,4 +95,6 @@ interface DataSharingStrategy {
   fun onResume(isScanning: Boolean = false)
 
   fun onPause()
+
+  fun initChannel(onDeviceFound: OnDeviceFound, onConnected: DataSharingStrategy.PairingListener)
 }
